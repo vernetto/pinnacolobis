ccca6b725ef557d4d2a4b55009f0dd938cd53dd5
vernetto <publicpierre@gmail.com>
2019-09-07 14:14:45 +0200

    test adjusted

--- src/test/java/org/pierre/pinnacolo/ManoDiCarteTest.java
+++ src/test/java/org/pierre/pinnacolo/ManoDiCarteTest.java
@@ -11,14 +11,24 @@ class ManoDiCarteTest {
     ManoDiCarte manoDiCarte = new ManoDiCarte();
     @Test
     void findAllTrisOrPoker() {
-        manoDiCarte.add(new Card(CardValues.V1, Seeds.CUORI));
-        manoDiCarte.add(new Card(CardValues.V1, Seeds.FIORI));
-        manoDiCarte.add(new Card(CardValues.V1, Seeds.PICCHE));
         manoDiCarte.add(new Card(CardValues.V1, Seeds.QUADRI));
         manoDiCarte.add(new Card(CardValues.V2, Seeds.QUADRI));
         manoDiCarte.add(new Card(CardValues.V3, Seeds.QUADRI));
-        List<CardCollection> result = manoDiCarte.findAllTrisOrPoker();
-        assertEquals(1, result.size());
+        List<CardCollection> result1 = manoDiCarte.findAllTrisOrPoker();
+        //make sure duplicates don't count'
+        assertEquals(0, result1.size());
+
+        manoDiCarte.add(new Card(CardValues.V1, Seeds.CUORI));
+        manoDiCarte.add(new Card(CardValues.V1, Seeds.FIORI));
+
+        List<CardCollection> result2 = manoDiCarte.findAllTrisOrPoker();
+        assertEquals(3, result2.get(0).size());
+
+        manoDiCarte.add(new Card(CardValues.V1, Seeds.PICCHE));
+        List<CardCollection> result3 = manoDiCarte.findAllTrisOrPoker();
+        assertEquals(1, result3.size());
+        assertEquals(4, result3.get(0).size());
+
 
     }
 }
